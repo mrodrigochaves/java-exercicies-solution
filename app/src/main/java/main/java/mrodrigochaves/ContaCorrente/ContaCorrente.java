d48301f2206b1a3cd2774676d071d6d5273c4542
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-12 22:17:19 -0300

    add getters and setters

--- app/src/main/java/main/java/mrodrigochaves/ContaCorrente/ContaCorrente.java
+++ app/src/main/java/main/java/mrodrigochaves/ContaCorrente/ContaCorrente.java
@@ -5,4 +5,38 @@ public class ContaCorrente {
     private Long id;
     private String nomeCliente;
     private double saldo;
+
+    public ContaCorrente(Long id, String nomeCliente, double saldo) {
+        this.id = id;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    
+
+    
 }
