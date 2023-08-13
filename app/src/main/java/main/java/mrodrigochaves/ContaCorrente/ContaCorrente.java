8a255abdefdf32a6016ee353f56b23dc3c173e68
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-08-12 22:25:28 -0300

    add metodo desposito

--- app/src/main/java/main/java/mrodrigochaves/ContaCorrente/ContaCorrente.java
+++ app/src/main/java/main/java/mrodrigochaves/ContaCorrente/ContaCorrente.java
@@ -40,5 +40,14 @@ public class ContaCorrente {
         this.nomeCliente = novoNome;
     }
 
+    public void deposito(double valor) {
+        if (valor > 0) {
+            saldo += valor;
+            System.out.println("Depósito de " + valor + " realizado com sucesso.");
+        } else {
+            System.out.println("Valor de depósito inválido.");
+        }
+    }
+
     
 }
